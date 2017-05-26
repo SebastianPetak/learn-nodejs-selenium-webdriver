a8091823d583d6bcd7fe98bd25f46c5ff6e953fe
Sebastian Petak <sebastianpetak@gmail.com>
2017-05-26 18:56:58 -0500

    Automate a search and verify it worked

--- server.js
+++ server.js
@@ -11,5 +11,23 @@ driver.manage().window().maximize();
 driver.manage().deleteAllCookies();
 // navigate to google.com
 driver.get('http://www.google.com/');
+// search webdriver in google
+driver.findElement(By.name('q')).sendKeys('webdriver');
+// click on search button
+driver.findElement(By.name('btnG')).click();
+// wait for page title
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+
+var promise = driver.getTitle();
+
+promise.then(function(title) {
+	console.log('Title is: ' + title);
+})
+
+driver.getTitle()
+.then(function(title) {
+	console.log('Title is still ' + title);
+})
+
 // close browser
-driver.quite();
+driver.quit();
