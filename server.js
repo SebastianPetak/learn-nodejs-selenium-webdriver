e39a9429361dce4dd587dade623d851e05ff25a3
Sebastian Petak <sebastianpetak@gmail.com>
2017-05-26 20:21:42 -0500

    Begin rewrite of program to search and click iolearn.com link

--- server.js
+++ server.js
@@ -10,12 +10,32 @@ driver.manage().window().maximize();
 // delete all cookies
 driver.manage().deleteAllCookies();
 
-/* Count how many internal links are on the /wiki/Wiki page */
+/* search for, find, and click, iolearn.com */
 
-driver.get('http://en.wikipedia.org/wiki/Wiki').then(function() {
-	console.log('find elements by css class');
-	return driver.findElements(By.css('[href^="/wiki/"]'));
-}).then(function(links) {
-	console.log('Found', links.length, 'wiki links');
+driver.get('http://google.com').then(function() {
+	console.log('find search bar');
+	return driver.findElement(By.name('q'));
+}).then(function(q) {
+	console.log('type iolearn.com Video Tutorials into google search');
+	q.sendKeys('iolearn.com Video Tutorials');
+}).then(function() {
+	console.log('find the search submit button');
+	return driver.findElement(By.name('btnG'));
+}).then(function(btnG) {
+	console.log('click the submit button for the google search');
+	btnG.click();
+}).then(function() {
+	console.log('wait for title to be "iolearn.com Video Tutorials - Google Search"');
+	driver.wait(until.titleIs('iolearn.com Video Tutorials - Google Search'), 1000);
+}).then(function() {
+	console.log('Find link to iolearn.com');
+	return driver.findElements(By.css('[href="http://iolearn.com/"]'));
+}).then(function(results) {
+	console.log('return first result')
+	return results[0];
+}).then(function(link) {
+	console.log('click link');
+	link.click();
+}).then(function() {
 	driver.quit();
-});
+})
